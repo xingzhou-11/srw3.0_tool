9d90ad40e871ba8a5aae693417d4e724f64ad9af
tianyi.kong <tianyi.kong@hcrobots.com>
2023-11-17 13:48:47 +0800

    实现读取机器人状态

--- main.py
+++ main.py
@@ -1,32 +1,69 @@
+import socket
 import PySimpleGUI as sg
 
-def check_robot_status():
+from LogTool import LogTool
+from threading import Thread
+from protobuf import zigbee_command, event_enum
+from srw.CommandData import CommandData
+
+def check_robot_status(action: zigbee_command, ip, port):
     """Check if the robot is in an error state
     """
-    pass
+    address = (ip, int(port))
+    action.address = address
 
-def robot_advance():
+    test_command = CommandData(seq=0)
+    test_command.set_status_command()
+    action.command_list.append(test_command.get_command())
+
+def robot_advance(client_socket):
     """Move the robot forward
     """
     print('机器人前进')
 
-def robot_retreat():
+def robot_retreat(client_socket):
     """Move the robot backward
     """
     pass
 
-def robot_stop():
+def robot_stop(client_socket):
     """Stop the robot
     """
     pass
 
 def main():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket.settimeout(2)
+
+    srw_action = zigbee_command()
+    proto_logger = LogTool("proto", "proto.log")
+
+    tsend = Thread(target=srw_action.send, args=[proto_logger.get_logger(), client_socket, srw_action.command_list])
+    trecv = Thread(target=srw_action.recv, args=[proto_logger.get_logger(), client_socket, event_enum])
+    tsend.daemon = True
+    trecv.daemon = True
+    tsend.start()
+    trecv.start()
+    
     # 设置主题 Dark or Light
     sg.theme('Light Green')
 
-    layout = [  [sg.Output(size=(100, 30))],
-                [sg.Button('查看机器人状态'), sg.Button('机器人前进'), sg.Button('机器人后退'), sg.Button('机器人停止'), sg.Button('机器人传感器验证')]
-                ]
+    Text_font = ('Consolas', 15)
+
+    layout = [
+        [sg.Output(size=(70, 20), font=Text_font)],
+        [sg.Text('网关IP地址:', font=Text_font), 
+         sg.Input(size=(20, 1), key='ip', default_text='10.0.64.14', font=Text_font), 
+         sg.Text('端口号:', font=Text_font), 
+         sg.Input(size=(20, 1), key='port', default_text='4008', font=Text_font)
+         ],
+        [sg.Button('查看机器人状态', font=Text_font), 
+         sg.Button('机器人前进', font=Text_font), 
+         sg.Button('机器人后退', font=Text_font), 
+         sg.Button('机器人停止', font=Text_font), 
+         sg.Button('机器人传感器验证', font=Text_font)
+         ],
+    ]
 
     # 创造窗口
     window = sg.Window('robot tool', layout)
@@ -38,7 +75,7 @@ def main():
             break
         if event == '查看机器人状态':
             print('查看机器人状态')
-            check_robot_status()
+            check_robot_status(srw_action, values['ip'], values['port'])
         if event == '机器人前进':
             robot_advance()
         if event == '机器人后退':
@@ -51,4 +88,6 @@ def main():
     window.close()
 
 if __name__ == '__main__':
+    
+    
     main()
--- protobuf.py
+++ protobuf.py
@@ -1,10 +1,14 @@
-import socket
 import crcmod
-import time
+import json
 from enum import Enum
-from protobuf import zigbee_pb2
+from srw import srw_pb2
 from google.protobuf.json_format import MessageToJson
 
+
+def status_analysis(data):
+    json_obj = json.loads(data)
+    state = json_obj["status"]["smlState"]
+    print(f'机器人状态: {state}')
 
 class event_enum(Enum):
     ACK_EVENT = {'val': 0, 'analysis': 1}
@@ -13,58 +17,47 @@ class event_enum(Enum):
     ERROR_EVENT = {'val': 3, 'analysis': 1}
     HEALTH_EVENT = {'val': 4, 'analysis': 1}
     INFO_EVENT = {'val': 5, 'analysis': 1}
-    STATUS_EVENT = {'val': 6, 'analysis': 1}
+    STATUS_EVENT = {'val': 6, 'analysis': status_analysis}
     CONFIG_EVENT = {'val': 7, 'analysis': 1}
     RESET_EVENT = {'val': 8, 'analysis': 1}
     HEART_BEAT_EVENT = {'val': 9, 'analysis': 1}
 
-command_list = []
+class zigbee_command:
+    def __init__(self):
+        self.command_list = []
+        self.address = None
 
-def append_crc(data: bytes) -> bytes:
-    # 定义 CRC 校验参数
-    crc16 = crcmod.predefined.Crc('kermit')
-    return data + crc16.new(data).crcValue.to_bytes(2, byteorder='little')
+    def append_crc(self, data: bytes) -> bytes:
+        # 定义 CRC 校验参数
+        crc16 = crcmod.predefined.Crc('kermit')
+        return data + crc16.new(data).crcValue.to_bytes(2, byteorder='little')
 
-def send(log, client_socket, command_list):
-    while True:
-        if command_list:
-            command = command_list.pop(0)
-            command_str = append_crc(command.SerializeToString())
-            for v in devices.values():
-                client_socket.sendto(command_str, (udp_addr, v))
-                log.info(f"sent to {v}: {command_str.hex()}")
+    def send(self, log, client_socket, command_list):
+        while True:
+            if command_list:
+                command = command_list.pop(0)
+                command_str = self.append_crc(command.SerializeToString())
+                client_socket.sendto(command_str, self.address)
+                log.info(f"sent to {self.address[1]}: {command_str.hex()}")
                 log.info(MessageToJson(command))
 
-def recv(log, client_socket, event_list, flag):
-    event = zigbee_pb2.Event()
-    while True:
-        try:
-            raw_data, addr = client_socket.recvfrom(1024)
-            data = raw_data[:len(raw_data) - 2]
-            if append_crc(data) != raw_data:
-                log.warning(f"crc failed: {raw_data.hex()}")
-                continue
+    def recv(self, log, client_socket, event_list):
+        event = srw_pb2.Event()
+        while True:
+            try:
+                raw_data, addr = client_socket.recvfrom(1024)
+                data = raw_data[:len(raw_data) - 2]
+                if self.append_crc(data) != raw_data:
+                    log.warning(f"crc failed: {raw_data.hex()}")
+                    continue
 
-            event.ParseFromString(data)
-            json_obj = MessageToJson(event)
-            log.info(f"recv from {addr}: {data.hex()}")
-            log.info(json_obj)
+                event.ParseFromString(data)
+                json_obj = MessageToJson(event)
+                log.info(f"recv from {addr}: {data.hex()}")
+                log.info(json_obj)
 
-            for i in event_list:
-                if event.type == i.value['val']:
-                    i.value['analysis']
-        except:
-            pass
-
-if __name__ == "__main__":
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client_socket.settimeout(2)
-
-    tsend = Thread(target=send, args=[proto_logger.get_logger(), client_socket, command_list])
-    trecv = Thread(target=recv, args=[proto_logger.get_logger(), client_socket, event_list, flag])
-    tsend.daemon = True
-    trecv.daemon = True
-    tsend.start()
-    trecv.start()
-
-
+                for i in event_list:
+                    if event.type == i.value['val']:
+                        return i.value['analysis'](json_obj)
+            except:
+                pass
